c0b88df0695fcd51cf28954d8dc5de8580a8d7eb
MohammadJavad Shahbazi <Mojavadssh@Gmail.com>
2025-05-04 11:16:42 +0330

    Time: 0 ms (100%), Space: 17.7 MB (53.72%) - LeetHub

--- 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
+++ 2337-remove-digit-from-number-to-maximize-result/2337-remove-digit-from-number-to-maximize-result.py
@@ -3,7 +3,7 @@ class Solution:
         max_result = ""
         for i in range(len(number)):
             if number[i] == digit:
-                new_number = number[:i] + number[i+1:]  # Remove the digit at index i
-                if new_number > max_result:  # Lexicographical comparison works for numbers as strings
+                new_number = number[:i] + number[i+1:]  
+                if new_number > max_result:  
                     max_result = new_number
         return max_result
